c03c58339def458f0b29153edcfb82985b7286d8
dscibot <contact@dscibot.ovh>
2025-05-15 21:24:55 +0200

    webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,5 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-
-  config.module.rules.push({
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  });
-
-  config.devServer = {
-    ...config.devServer,
-    historyApiFallback: true, // utile pour React Navigation web ou React Router
-  };
-
   return config;
 };
