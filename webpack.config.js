ab6a0db62c15e70dde3711bc15554b7508256c87
dscibot <contact@dscibot.ovh>
2025-05-15 21:21:25 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
-const { withExpoWebpack } = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  return await withExpoWebpack(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
+
+  config.devServer = {
+    ...config.devServer,
+    historyApiFallback: true, // utile pour React Navigation web ou React Router
+  };
+
+  return config;
 };
